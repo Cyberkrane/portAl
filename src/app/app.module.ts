0e6c4a2ee592442a5f78b92c617c407b5d16c3be
cyberkrane <cyberkranegmail.com>
2023-01-27 22:12:14 -0300

    lanzamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StoriesComponent } from './pages/stories/stories.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { CardComponent } from './pages/card/card.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { SuggestionsComponent } from './pages/suggestions/suggestions.component';
+import { NaveComponent } from './pages/nave/nave.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    StoriesComponent,
+    CardsComponent,
+    CardComponent,
+    PerfilComponent,
+    SuggestionsComponent,
+    NaveComponent
   ],
   imports: [
     BrowserModule,
